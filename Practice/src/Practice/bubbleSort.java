ce33994b3b3ddd2b62506ec01d33e84cf0d0643f
pankajchauhano7 <pcpankajo7@gmail.com>
2025-05-06 11:46:15 +0530

    bubble sort

--- Practice/src/Practice/bubbleSort.java
+++ Practice/src/Practice/bubbleSort.java
@@ -7,7 +7,7 @@ public class bubbleSort {
 	public static void bubSort(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - 1 - i; j++) {
-				if (arr[j] < arr[j + 1]) {
+				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
